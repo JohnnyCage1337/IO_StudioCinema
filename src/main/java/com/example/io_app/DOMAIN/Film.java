a104d3d99ddb03581ac7ee05b4665a153b42b141
Maks <maksymilian.lyko.pol@gmail.com>
2025-01-22 14:43:34 +0100

    Dalsze zmiany

--- src/main/java/com/example/io_app/DOMAIN/Film.java
+++ src/main/java/com/example/io_app/DOMAIN/Film.java
@@ -1,78 +1,78 @@
-package com.example.io_app.DOMAIN;
+    package com.example.io_app.DOMAIN;
 
-public class Film {
+    public class Film {
 
-    private String name;
-    private int duration;
-    private String description;
-    private String genre;
-    private String rate;
-    private String director;
-    private String language;
+        private String name;
+        private int duration;
+        private String description;
+        private String genre;
+        private String rate;
+        private String director;
+        private String language;
 
-    public Film(String name, int duration, String description, String genre, String rate, String director, String language){
-        this.name = name;
-        this.duration = duration;
-        this.description = description;
-        this.genre = genre;
-        this.rate = rate;
-        this.director = director;
-        this.language = language;
+        public Film(String name, int duration, String description, String genre, String rate, String director, String language){
+            this.name = name;
+            this.duration = duration;
+            this.description = description;
+            this.genre = genre;
+            this.rate = rate;
+            this.director = director;
+            this.language = language;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getDuration() {
+            return duration;
+        }
+
+        public void setDuration(int duration) {
+            this.duration = duration;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getGenre() {
+            return genre;
+        }
+
+        public void setGenre(String genre) {
+            this.genre = genre;
+        }
+
+        public String getRate() {
+            return rate;
+        }
+
+        public void setRate(String rate) {
+            this.rate = rate;
+        }
+
+        public String getDirector() {
+            return director;
+        }
+
+        public void setDirector(String director) {
+            this.director = director;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getDuration() {
-        return duration;
-    }
-
-    public void setDuration(int duration) {
-        this.duration = duration;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getRate() {
-        return rate;
-    }
-
-    public void setRate(String rate) {
-        this.rate = rate;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-}
---
+++ src/main/java/com/example/io_app/DTO/SessionDTO.java
@@ -0,0 +1,4 @@
+package com.example.io_app.DTO;
+
+public class SessionDTO {
+}
